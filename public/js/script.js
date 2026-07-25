004037a873bf280bd4ef079414b695b6887b82b5
Claudemir Ferreira <ferreira.claudemir0425@gmail.com>
2026-07-25 16:08:29 -0300

    Criado layout inicial do catálogo

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,81 @@
-console.log("Catalogo Zap iniciado.");
+const produtos = [
+
+{
+
+nome:"Nike Air Max",
+
+preco:399.90,
+
+imagem:"https://picsum.photos/400/300?1"
+
+},
+
+{
+
+nome:"Adidas Run",
+
+preco:299.90,
+
+imagem:"https://picsum.photos/400/300?2"
+
+},
+
+{
+
+nome:"Puma Sport",
+
+preco:279.90,
+
+imagem:"https://picsum.photos/400/300?3"
+
+},
+
+{
+
+nome:"Olympikus",
+
+preco:199.90,
+
+imagem:"https://picsum.photos/400/300?4"
+
+}
+
+];
+
+const area=document.getElementById("produtos");
+
+produtos.forEach(produto=>{
+
+area.innerHTML+=`
+
+<div class="col-md-4">
+
+<div class="card">
+
+<img src="${produto.imagem}">
+
+<div class="card-body">
+
+<h5>${produto.nome}</h5>
+
+<h4 class="text-success">
+
+R$ ${produto.preco.toFixed(2)}
+
+</h4>
+
+<button class="btn btn-success w-100">
+
+Adicionar ao carrinho
+
+</button>
+
+</div>
+
+</div>
+
+</div>
+
+`;
+
+});
